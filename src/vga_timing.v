51d93622de743c0db3f0f2a5533c127366b1f8c4
Noah Harman <noahharman190@gmail.com>
2025-10-18 19:38:36 -0400

    oct 18

--- src/vga_timing.v
+++ src/vga_timing.v
@@ -1,8 +1,16 @@
 module vga_timing_640x480(
     input wire clk_pix,
-    input wire clk_pix,
+    input wire resetn,
     output wire [9:0] hcount,
-    output wire [9:0] hcount,
-    output wire [9:0] hcount,
-    output wire [9:0] hcount,
+    output wire [9:0] vcount,
+    output wire hsync,
+    output wire de,
 )
+
+always @(posedge clk_pix or posedge resetn) begin 
+    if(resetn) begin
+
+    end else begin 
+        hcount <= hcount + 1;
+    end
+end
