73bbbe5a6f2e8788cfb818c56f2294a8bed65c37
Noah Harman <noahharman190@gmail.com>
2025-10-18 19:21:54 -0400

    oct 18

---
+++ src/vga_timing.v
@@ -0,0 +1,8 @@
+module vga_timing_640x480(
+    input wire clk_pix,
+    input wire clk_pix,
+    output wire [9:0] hcount,
+    output wire [9:0] hcount,
+    output wire [9:0] hcount,
+    output wire [9:0] hcount,
+)
