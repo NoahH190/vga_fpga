db3a00618c2bf6448e11ca178044f95f5fbeb513
Noah Harman <noahharman190@gmail.com>
2025-10-22 13:44:58 -0400

    oct 22

--- tb/timing_tb.v
+++ tb/timing_tb.v
@@ -1,12 +1,22 @@
-module vga_timing_top;
+module vga_timing_test;
     reg clk_pix, resetn;  
     wire[9:0] hcount, vcount;
     wire hsync, locked;
     vga_timing_block vt1 (clk_pix, resetn, hcount, vcount, hsync, locked);
  
 initial begin 
-    $dumpfile("vga_timing");
+    $dumpfile("vga_timing.vcd");
     $dumpvars(0,vga_timing_test);
+    
+    clk_pix = 0; b = 0; #10;
+    clk_pix = 0; b = 1; #10;
+    clk_pix = 1; b = 0; #10;
+    clk_pix = 1; b = 1; #10;
+
+    $finish;
+end
+
+
 
 
 vga_timing_640x480 dut (
