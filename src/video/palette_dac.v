ed886ab0aa428fc84b7432a95514ecc27e714662
Noah Harman <noahharman190@gmail.com>
2025-10-26 14:36:43 -0400

    oct 26

--- src/video/palette_dac.v
+++
@@ -1,8 +0,0 @@
-module palette_dac (
-    input wire clk_pix,
-    input wire resetn,
-    input wire [7:0] rgb_index,
-    output reg [2:0] r,
-    output reg [2:0] b,
-    output reg [2:0] g,
-)
--- src/video/pixel_clk_gen.v
+++ src/video/pixel_clk_gen.v
@@ -1,26 +1,25 @@
 module pixel_clk_gen(
     input wire clk_in,  //system clock, 100 MHz
     input wire resetn,
-    output wire clk_pix,   //25 MHz (should be 25.175 but wtv)
-    output wire locked
+    output reg clk_pix,   //25 MHz (should be 25.175 but wtv)
 );
 
 // create a clock divider to get a pixel clock of 25 MHz, divide by 4
 
-    localparam divisor = 4;
+    localparam divisor = 2;
     reg [31:0] counter = 32'd0;
 
-    always @(posedge clk_in or posedge reset) begin
-        if (reset) begin 
+    always @(posedge clk_in or negedge resetn) begin
+        if (!resetn) begin 
             counter <= 32'd0;
-            clock_pix <= 1'b0;
+            clk_pix <= 1'b0;
         end else begin 
-            counter <= counter + 1;
             if(counter >= divisor - 1) begin
-                clock_pix <= ~clock_pix;
+                clk_pix <= ~clk_pix;
                 counter <= 32'd0;
-            end
+            end else 
+                counter <= counter + 1;
         end
     end
-
+    
 endmodule 
