4e5d8e68ce417a3c3a987a043a9b1305e0f5f61a
Noah Harman <noahharman190@gmail.com>
2025-11-07 18:35:29 -0500

    Nov 7

--- src/top/vga_top copy.v
+++ src/top/vga_top copy.v
@@ -1,22 +1,25 @@
 module vga_top (
     input wire clk_pix,     // pixel clock ~25 MHz
     input wire resetn,
-    output wire hsync; 
-    output wire vsync;
+    output wire hsync,
+    output wire vsync,
     output reg [2:0] rgb_r,       // 3 bits red
     output reg [2:0] rgb_g,       // 3 bits green
     output reg [2:0] rgb_b
 )
 
-    wire vga_vid;
+
+    wire [9:0] hcount:
+    wire [9:0] vcount:
+    wire de;
     wire vga_hsync;
     wire vga_vsync;
     
     vga_timing vga_timing_1 (
         .clk_pix(clk_pix),
         .resetn(resetn),
-        .hcount(),
-        .vcount(),
+        .hcount(hcount),
+        .vcount(vcount),
         .hsync(hsync),
         .vsync(vsync),
         .de()
